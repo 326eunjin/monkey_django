cfac193f4647514b576ea975fe424ec66273490e
326eunjin <326eunjin@naver.com>
2022-08-13 17:46:36 +0900

    [fix] login

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,10 @@
 
 from django.urls import path
 from django.contrib.auth import views as auth_views
-
+import users.views
 app_name = "users"
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.jsp'), name='login'),
+    path('login/', auth_views.LoginView.as_view(
+        template_name='users/login.jsp'), name='login'),
+    path('signup/', users.views.signup, name='signup')
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def signup(request):
-    return render(request, '../templates/signup.jsp')
+    return render(request, 'users/signup.jsp')
